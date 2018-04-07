49e359e34b7a5d0f067e96507610b42c6328d20a
Jian Han <jianhan@mail.com>
2018-04-08 00:08:25 +1000

    [DEV] Added material layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import { ApolloProvider } from "react-apollo";
+import {ApolloProvider} from "react-apollo";
 import ApolloClient from "apollo-boost";
-import ExchangeRates from "./ExchangeRates"
+import Home from "./components/Home";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
-const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
-});
+const client = new ApolloClient({uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"});
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <ExchangeRates />
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        </div>
+        <Router>
+          <Route exact path="/" component={Home}/>
+        </Router>
       </ApolloProvider>
     );
   }
